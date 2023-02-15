97eb093a0253141e9acff30031d8763646541ace
Waqas Mustafa <malik.waqas.09@hotmail.com>
2023-02-15 18:47:26 +0500

    test exception

--- app/Console/Commands/debugLogic.php
+++ app/Console/Commands/debugLogic.php
@@ -37,6 +37,8 @@ class debugLogic extends Command
         ini_set('display_startup_errors', 1);
         error_reporting(E_ALL);
 
+        app('sentry')->captureException("test error");
+        die;
         $string = "BHXZ211860007\x00";
         $a = preg_replace('/[[:cntrl:]]/', '', $string);
 
