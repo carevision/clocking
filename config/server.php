adacdaa44e4015a9fd7aca680338e24a02196d3e
Waqas Mustafa <malik.waqas.09@hotmail.com>
2023-02-24 14:46:06 +0500

    linksdev branch

--- config/server.php
+++ config/server.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'url'   => env('url', 'https://atif.care-vision.co.uk/api/v2/'),
-    'url_sync' => env('url_sync', 'https://atif.care-vision.co.uk/api/v2/storeClocking'),
-    'database_backup' => env('database_backup', 'https://atif.care-vision.co.uk/api/clocking/database/backup'),
-    'login_url' => env('login_url', 'https://atif.care-vision.co.uk/api/login')
+    'url'   => env('url', 'https://jawa.linksdev.co.uk/api/v2/'),
+    'url_sync' => env('url_sync', 'https://jawa.linksdev.co.uk/api/v2/storeClocking'),
+    'database_backup' => env('database_backup', 'https://jawa.linksdev.co.uk/api/clocking/database/backup'),
+    'login_url' => env('login_url', 'https://jawa.linksdev.co.uk/api/login')
 ];
